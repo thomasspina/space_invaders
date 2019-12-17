787502fa72e27674bc460381e6fe024cd850ffa0
Thomas Spina <thomas@thomasspina.com>
2019-12-17 09:55:56 -0700

    Update BarrierSprite.java
    Modified the barrier sprite for my purposes, no need for images as all barriers are invisible.

--- sprites/BarrierSprite.java
+++ sprites/BarrierSprite.java
@@ -1,35 +1,15 @@
 import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class BarrierSprite extends StaticSprite {
-
-	private static Image image = null;
-	
-	public BarrierSprite(double minX, double minY, double maxX, double maxY, boolean showImage) {
-		
-		if (image == null && showImage) {
-			try {
-				image = ImageIO.read(new File("res/blue-barrier.png"));
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}		
-		}
-		
+	public BarrierSprite(double minX, double minY, double maxX, double maxY) {
 		this.minX = minX;
 		this.minY = minY;
 		this.maxX = maxX;
 		this.maxY = maxY;
-		this.showImage = showImage;
-		
-		
-		
 	}
 	
 	@Override
 	public Image getImage() {
-		return image;
+		return null;
 	}
 }
