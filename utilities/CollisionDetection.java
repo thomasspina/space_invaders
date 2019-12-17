04d8ccc9a13910e9134178095958d8a622692777
Thomas Spina <thomas@thomasspina.com>
2019-12-17 09:58:59 -0700

    Update CollisionDetection.java
    Modified the collision detection class for my purposes. I did not need all the methods. Might not need all that are left either.

--- utilities/CollisionDetection.java
+++ utilities/CollisionDetection.java
@@ -1,7 +1,3 @@
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-
 public class CollisionDetection {
 	public static boolean overlaps(double a_left, double a_top, double a_right, double a_bottom, double b_left, double b_top, double b_right, double b_bottom) {
 		boolean a_to_left_of_b = (a_right < b_left);
@@ -23,47 +19,6 @@ public class CollisionDetection {
 		} else {
 			return false;	    	
 		}
-	}
-	
-	public static boolean pixelBasedOverlaps(Sprite spriteA, Sprite spriteB) {
-		if (overlaps(spriteA.getMinX(), spriteA.getMinY(), spriteA.getMaxX(), spriteA.getMaxY(), 
-				spriteB.getMinX(), spriteB.getMinY(), spriteB.getMaxX(), spriteB.getMaxY()) == false) {
-			return false;
-		}
-		
-		BufferedImage bufferedA = (BufferedImage) spriteA.getImage();
-		BufferedImage bufferedB = (BufferedImage) spriteB.getImage();
-		
-		int offsetX = (int) (spriteB.getMinX() - spriteA.getMinX());
-		int offsetY = (int) (spriteB.getMinY() - spriteA.getMinY());
-		
-		int left = Math.max(0, (int) (offsetX));
-		int top =  Math.max(0, (int) (offsetY));
-		int right = (int) (spriteA.getWidth() - Math.max(0, spriteA.getMaxX() - spriteB.getMaxX()));
-		int bottom = (int) (spriteA.getHeight() - Math.max(0, spriteA.getMaxY() - spriteB.getMaxY()));
-		
-		double scaleXA = bufferedA.getHeight() / (float)spriteA.getWidth();
-		double scaleYA = bufferedA.getHeight() / (float)spriteA.getHeight();
-		double scaleXB = bufferedB.getHeight() /  (float)spriteB.getWidth();
-		double scaleYB = bufferedB.getHeight() /  (float)spriteB.getHeight();
-
-		for (int x = left; x < right; x++) {
-			for (int y = top; y < bottom; y++) {
-				int xA = (int)(x * scaleXA);
-				int yA = (int)(y * scaleYA);				
-				int xB = (int) ((x - offsetX) * scaleXB);
-				int yB = (int) ((y - offsetY) * scaleYB);
-				if ((xB >= 0) && (yB >= 0) && (yB < bufferedB.getWidth()) && (yB < bufferedB.getHeight())) {
-					int pixelA = bufferedA.getRGB(xA, yA);
-					int pixelB = bufferedB.getRGB(xB, yB);
-					if ((pixelA>>>24 > 0x00) && (pixelB>>>24 > 0x00)) {
-						return true;
-					}
-				}
-			}
-		}
-		
-		return false;
 	}
 
 	public static boolean covers (double a_left, double a_top, double a_right, double a_bottom, double b_left, double b_top, double b_right, double b_bottom) {
