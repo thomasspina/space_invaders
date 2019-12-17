f263287aada10ed89ec54db1559db9a2d25c2831
Thomas Spina <thomas@thomasspina.com>
2019-12-17 10:04:32 -0700

    Update MenuScreen.java
    Modified the class in order to add the limits for the sprites and add some aliens for testing, all is working well.

--- screens/MenuScreen.java
+++ screens/MenuScreen.java
@@ -1,8 +1,20 @@
-
 public class MenuScreen extends Screen {
 	
 	public MenuScreen() {
 		super();
+		setXCenter(0);
+		setYCenter(0);
+		
+		BarrierSprite rightBarrier = new BarrierSprite(350, -250, 340, 270);
+		BarrierSprite leftBarrier = new BarrierSprite(-363, -250, -353, 270);
+		AlienSprite alien = new AlienSprite(1, 200, 0);
+		AlienSprite alien2 = new AlienSprite(1, 150, 0);
+		
+		activeSprites.add(alien2);
+		activeSprites.add(alien);
+		
+		staticSprites.add(rightBarrier);
+		staticSprites.add(leftBarrier);
 	}
 
 	@Override
@@ -12,6 +24,7 @@ public class MenuScreen extends Screen {
 
 	@Override
 	public void update(KeyboardInput keyboard, long actual_delta_time) {
-		
+		updateSprites(keyboard, actual_delta_time);
+		disposeSprites();
 	}
 }
