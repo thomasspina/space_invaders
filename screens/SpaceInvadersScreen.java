85f829b8b2fe94cc7d82386137165e1ad10f096a
Thomas Spina <thomas@thomasspina.com>
2019-12-18 11:28:57 -0700

    Update SpaceInvadersScreen.java
    Moved the code from the menu screen over to this game screen

--- screens/SpaceInvadersScreen.java
+++ screens/SpaceInvadersScreen.java
@@ -1,8 +1,31 @@
-
 public class SpaceInvadersScreen extends Screen {
 
 	public SpaceInvadersScreen() {
 		super();
+		setXCenter(0);
+		setYCenter(0);
+		
+		BarrierSprite rightBarrier = new BarrierSprite(350, -250, 340, 270);
+		BarrierSprite leftBarrier = new BarrierSprite(-363, -250, -353, 270);
+		
+		int alienType = 3;
+		int yPosition = 0;
+		for (int i = 0; i < 5; i++) {
+			int xPosition = -300;
+			for (int j = 0; j < 11; j++) {
+				AlienSprite alien = new AlienSprite(alienType, xPosition, yPosition);
+				activeSprites.add(alien);
+				xPosition += 40;
+			}
+			yPosition -= 40;
+			if (i == 1 || i == 3) {
+				alienType--;
+			}
+		}
+		
+		
+		staticSprites.add(rightBarrier);
+		staticSprites.add(leftBarrier);
 	}
 	
 	@Override
@@ -12,7 +35,8 @@ public class SpaceInvadersScreen extends Screen {
 
 	@Override
 	public void update(KeyboardInput keyboard, long actual_delta_time) {
-		
+		updateSprites(keyboard, actual_delta_time);
+		disposeSprites();
 	}
 
 }
